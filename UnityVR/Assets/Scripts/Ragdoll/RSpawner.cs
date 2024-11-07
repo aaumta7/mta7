3ab7c80d4ca61767ea6a541ac02d1fefba256b96
Shiro <kodokododk@gmail.com>
2024-11-07 13:22:43 +0100

    Reset button

--- UnityVR/Assets/Scripts/Ragdoll/RSpawner.cs
+++ UnityVR/Assets/Scripts/Ragdoll/RSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RSpawner : MonoBehaviour
 {
@@ -18,5 +19,10 @@ public class RSpawner : MonoBehaviour
             Destroy(thisDoll);
             thisDoll = Instantiate(RagdollPrefab, transform.position, Quaternion.identity);
         }
+    }
+
+    public void GrabActivated(ActivateEventArgs args)
+    {
+        RespawnDoll();
     }
 }
