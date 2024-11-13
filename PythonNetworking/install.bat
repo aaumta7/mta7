cfe641356bc701dd936b9a1865462ed9e084a522
Mathilde <mjesp20@student.aau.dk>
2024-11-13 09:24:07 +0100

    Cleaned up py networking and added gitignore

--- PythonNetworking/install.bat
+++ PythonNetworking/install.bat
@@ -1,7 +1,7 @@
 python -m venv ./venv
 call ./venv/Scripts/activate.bat
 pip install pipreqs
-pipreqs ./--force
+pipreqs ./ --force
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install opencv-python
 pip install -r requirements.txt
