f9b2890b51f288eb546a9a8c955505525f7b4490
Mathilde <mjesp20@student.aau.dk>
2024-11-19 10:37:08 +0100

    Made Email display with json reader. currently operated by o,p and l but public funtions for buttons exist

--- UnityVR/Assets/Scripts/jsonReader.cs
+++ UnityVR/Assets/Scripts/jsonReader.cs
@@ -3,23 +3,84 @@ using System.Collections;
 using System.IO;
 using TMPro;
 using UnityEngine.Rendering;
-
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class jsonReader : MonoBehaviour
 {
-
+    public int emailsDaily;
     private JsonData jsonData;
-
-    void Start()
+    List<Character> currEmails;
+    public TMP_Text headline,fromline,body,prompt,requirements,payment;
+    Character curr;
+    
+    List<Character> newEmails()
     {
-        string jsonString = File.ReadAllText(Application.streamingAssetsPath + "/test.json");
-        jsonData = JsonUtility.FromJson<JsonData>(jsonString);
-        /* example to display email
-        headline.text = jsonData.Characters[0].Emails[0].Headline;
-        body.text = jsonData.Characters[0].Emails[0].Text;
-        prompt.text = jsonData.Characters[0].Prompts[0];
-        string req = jsonData.Characters[0].Emails[0].Requirement;
+        if (jsonData.Characters.Length==0)
+        {
+            return new List<Character>();
+        }
+
+        int emails = 0;
+        List<Character> characters = new List<Character>() ;
+        System.Random random = new System.Random();
+        if (jsonData.Characters.Length >= emailsDaily)
+        {
+            do
+            {
+                int rnd = random.Next(jsonData.Characters.Length);
+                if (!characters.Contains(jsonData.Characters[rnd]))
+                {
+                    characters.Add(jsonData.Characters[rnd]);
+                    emails++;
+                }
+            } while (emails < emailsDaily);
+        }
+        else
+        {
+            characters = jsonData.Characters.ToList();
+        }
+
+        return characters;
+    }
+    public void next()
+    {
+        int index = currEmails.IndexOf(curr);
+        if (index == currEmails.Count-1 )
+        {
+            curr = currEmails[0];
+        }
+        else
+        {
+            curr =  currEmails[index+1];
+        }
+
+    }
+    public void prev()
+    {
+        int index = currEmails.IndexOf(curr);
+        if (index == 0)
+        {
+            curr = currEmails[currEmails.Count - 1];
+        }
+        else
+        {
+            curr = currEmails[index - 1];
+        }
+    }
+    void displayEmail()
+    {
+        
+        int index = jsonData.Characters.ToList().IndexOf(curr);
+        System.Random random = new System.Random();
+        int progress = jsonData.Characters[index].Progress;
+
+        headline.text = curr.Emails[progress].Headline;
+        fromline.text = curr.Name;
+        body.text = curr.Emails[progress].Text;
+        prompt.text = curr.Prompts[progress];
+        string req = curr.Emails[progress].Requirement;
         if (req != null) //sometimes null
         {
             requirements.text = req;
@@ -28,14 +89,75 @@ public class jsonReader : MonoBehaviour
         {
             requirements.text = "no requirement";
         }
-        payment.text = jsonData.Characters[0].Emails[0].Payment.ToString() + "grams of parmesan";
-        */
+        payment.text = curr.Emails[progress].Payment.ToString() + " grams of parmesan";
+    }
+    public void accept()
+    {
+        int prog = jsonData.Characters[jsonData.Characters.ToList().IndexOf(curr)].Progress;
+        prog++;
+        jsonData.Characters[jsonData.Characters.ToList().IndexOf(curr)].Progress = prog;
+        if (prog == jsonData.Characters[jsonData.Characters.ToList().IndexOf(curr)].Emails.Length)
+        {
+            List<Character> c = jsonData.Characters.ToList();
+            c.Remove(curr);
+            foreach (Character ch in c)
+            {
+                Debug.Log(ch.Name);
+            }
+            jsonData.Characters = c.ToArray();
+        }
+        
+        load();
+    }
+    void load()
+    {
+        currEmails = newEmails();
+        if (currEmails.Count == 0)
+        {
+            finish();
+            return;
+        }
+        System.Random random = new System.Random();
+
+        curr = currEmails[0];
+        displayEmail();
+
+    }
+
+    void Start()
+    {
+        string jsonString = File.ReadAllText(Application.streamingAssetsPath + "/test.json");
+
+        jsonData = JsonUtility.FromJson<JsonData>(jsonString);
+
+        load();
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            prev();
+            Debug.Log("prev");
+            displayEmail();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            next();
+            Debug.Log("next");
+            displayEmail();
+        }
+        if (Input.GetKeyDown(KeyCode.L)) 
+        {
+            accept();
+        }
     }
 
     [System.Serializable]
     public class Character
     {
         public string Name;
+        public int Progress;
         public string[] Prompts;
         public Email[] Emails;
     }
@@ -54,4 +176,15 @@ public class jsonReader : MonoBehaviour
     {
         public Character[] Characters;
     }
+    void finish()
+    {
+        headline.text = "finish";
+        fromline.text = "finish";
+        body.text = "finish";
+        prompt.text = "finish";
+        requirements.text = "finish";
+        payment.text = "finish";
+    }
+
+
 }
