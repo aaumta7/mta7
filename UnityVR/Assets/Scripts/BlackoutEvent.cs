fbe46c102c8538b125dca545632a136b7d735b4c
Shiro <43093549+Aphushiro@users.noreply.github.com>
2024-11-26 11:45:58 +0100

    Blackout shader + script

---
+++ UnityVR/Assets/Scripts/BlackoutEvent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackoutEvent : MonoBehaviour
+{
+    public Material mat;
+    float target = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat.SetFloat("_Brightness", 1.0f);
+    }
+
+    private void Update()
+    {
+        float cur = mat.GetFloat("_Brightness");
+
+        float setBright = Mathf.Clamp(Mathf.Lerp(cur, target, 0.1f), 0f, 1f);
+        mat.SetFloat("_Brightness", setBright);
+    }
+
+    public void DoFade ()
+    {
+        mat.SetFloat("_Brightness", 1.0f);
+        StartCoroutine(FadeInOut());
+    }
+    
+    IEnumerator FadeInOut()
+    {
+        target = 0.001f;
+        yield return new WaitForSeconds(1);
+        mat.SetFloat("_Brightness", 0.0f);
+
+        target = 1.0f;
+        yield return new WaitForSeconds(1);
+        mat.SetFloat("_Brightness", 1.0f);
+    }
+}
